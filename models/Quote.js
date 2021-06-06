824074dcebb455321d87c0f82417f367e02ada6c
CrgioYalux <randaledomman1@gmail.com>
2021-06-06 09:06:31 -0300

    created the quote model

---
+++ models/Quote.js
@@ -0,0 +1,27 @@
+const { Schema, model } = require('mongoose');
+
+// Schemes creation
+
+const quoteSchema = new Schema({
+	content: String,
+	date: Date,
+	author: String,
+});
+
+// This 'configuration' will be applied when creating and fetching the quotes
+
+quoteSchema.set('toJSON', {
+	transform: (document, returnedObject) => {
+		returnedObject.id = returnedObject._id;
+		delete returnedObject._id;
+		delete returnedObject.__v;
+	},
+});
+
+// Model Creation
+
+const Quote = model('Quote', quoteSchema);
+
+module.exports = {
+	Quote,
+};
